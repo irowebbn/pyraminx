011951647959396e9f000ff888c1c3ab16727750
Isaac Rowe <irowe.bbn@gmail.com>
2020-09-15 15:47:10 -0400

    Working version, log nodes expanded

--- autosolve.cpp
+++ autosolve.cpp
@@ -1,69 +1,104 @@
 #include <queue>
 #include <utility> 
+#include <fstream>
+#include <iostream>
 
 #include "Pyraminx.h"
 
+// Simple encapsulations for the moves we will apply to pyraminx
 struct move_type{
     Corner ref_corner;
     int layer;
     Direction dir;
 };
 
+// Nice names for our weird data structures
+// A list of moves that lead to a node
 typedef std::vector<move_type> move_list;
+// A node, which has an f value (cost) and a move list leading to it
 typedef std::pair<int, move_list> node;
 
+// Comparison operator for our min-heap
 struct Compare{
     bool operator()(node& p1, node& p2 ){
         return p1.first > p2.first;
     }
 };
 
+// Create a min-heap using a STL priority queue
 typedef std::priority_queue<node, std::vector<node>, Compare> min_heap;
 
+// Given a node, generate the children nodes, calculate their f values (costs), 
+// and put them in the min-heap
 void enqueue_children(min_heap& to_expand, const node& parent, Pyraminx& pyraminx);
+// Helper function for enqueue children that enqueues the children corresponding 
+// to all the possible moves from a given corner of the puzzle
 void enqueue_corner(min_heap& to_expand, const node& parent, Pyraminx& pyraminx, const Corner& ref_corner);
-bool goal_test(const node& current, Pyraminx& pyraminx);
+// Prints the move list of a node
 void print_move_list(const node& current);
+// Applies the move list needed to reach a node
 void apply_moves(const node& current, Pyraminx& pyraminx);
+// Undoes the move list needed to reach a node
 void undo_moves(const node& current, Pyraminx& pyraminx);
 
+// Driver for autosolve test
 int main(){
+    // Open file to write out number of expanded nodes:
+    std::ofstream logfile;
+    logfile.open ("log.txt", std::ios::out | std::ios::app );
+    logfile << "k, nodes expanded\n";
+    logfile.close();
+
     int k_start = 3;
     int k_stop = 20;
+    int num_instances = 5;
+    // For each k, solve 5 puzzles randomized with k moves
     for(int k = k_start; k <= k_stop; k++){
-        Pyraminx myPyraminx;
-        myPyraminx.scramble(k);
-        printf("Solving puzzle randomized with %i moves...\n", k);
-        myPyraminx.print();
-        int g = 0;
-        int h = myPyraminx.get_heuristic();
-        int f = g + h;
-        move_list moves_so_far;
-        min_heap to_expand;
-        to_expand.push(node(f, moves_so_far));
-        bool finished;
-        while(!to_expand.empty()){
-            node current = to_expand.top();
-            to_expand.pop();
-            finished = goal_test(current, myPyraminx);
-            if(finished){
-                // printf("Initial:\n");
-                // myPyraminx.print();
-                // apply_moves(current, myPyraminx);
-                // myPyraminx.print();
-                printf("Solved! Solution is: ");
-                print_move_list(current);
-                break;
+        float count = 0;
+        for(int i = 0; i < num_instances; i++){
+            Pyraminx myPyraminx;
+            myPyraminx.scramble(k);
+            printf("Solving puzzle randomized with %i moves...\n", k);
+            myPyraminx.print();
+
+            // Create and enqueue root node
+            int g = 0;
+            int h = myPyraminx.get_heuristic();
+            int f = g + h;
+            move_list moves_so_far;
+            min_heap to_expand;
+            to_expand.push(node(f, moves_so_far));
+
+            // Explore tree
+            bool finished;
+            while(!to_expand.empty()){
+                node current = to_expand.top();
+                to_expand.pop();
+                apply_moves(current, myPyraminx);
+                // Debug prints print_move_list(current);
+                finished = myPyraminx.is_solved();
+                if(finished){
+                    printf("Solved! Solution is: ");
+                    print_move_list(current);
+                    break;
+                }
+                else{
+                    count++;
+                    enqueue_children(to_expand, current, myPyraminx);
+                    undo_moves(current, myPyraminx);
+                }
             }
-            else{
-                // printf("Not solved. Reverting.\n");
-                enqueue_children(to_expand, current, myPyraminx);
-            }
-        }
-    }
-    
+        }// i for loop (5 instances)
+        // We reopen and close the file each time because we don't want to lose
+        // the data if termination is early
+        logfile.open ("log.txt", std::ios::out | std::ios::app );
+        float average = count / num_instances;
+        logfile << k << ", " << average << "\n";
+        logfile.close();
+    }// k for loop (k from 3 to 20) 
 }
 
+// Given a node, add nodes for all the possible move types.
 void enqueue_children(min_heap& to_expand, const node& parent, Pyraminx& pyraminx){
     for(int i = 0; i < 4; i++){
         Corner ref_corner;
@@ -86,6 +121,7 @@ void enqueue_children(min_heap& to_expand, const node& parent, Pyraminx& pyramin
     }
 }
 
+// For a given corner, create a node for all the possible layers and directions
 void enqueue_corner(min_heap& to_expand, const node& parent, Pyraminx& pyraminx, const Corner& ref_corner){
     // for each possible layer and direction:
     // apply move
@@ -98,8 +134,9 @@ void enqueue_corner(min_heap& to_expand, const node& parent, Pyraminx& pyraminx,
     int g_parent;
     int g_child;
     int f_child; 
-    for(int layer = 0; layer < 4; layer++){
-        for(int dir = 0; dir < 2; dir++){
+
+    for(int layer = 0; layer < 4; layer++){ // iterate layers
+        for(int dir = 0; dir < 2; dir++){ // iterate directions
             if(dir == 0){
                 move_dir = Direction::clockwise;
                 undo_dir = Direction::counterclockwise;
@@ -108,8 +145,9 @@ void enqueue_corner(min_heap& to_expand, const node& parent, Pyraminx& pyraminx,
                 move_dir = Direction::counterclockwise;
                 undo_dir = Direction::clockwise;
             }
-            pyraminx.turn_layer(ref_corner, layer, move_dir);
-            h_child = pyraminx.get_heuristic();
+            // Get cost by applying move to pyraminx
+            pyraminx.turn_layer(ref_corner, layer, move_dir); 
+            h_child = pyraminx.get_heuristic(); 
             g_parent = parent.second.size();
             g_child = g_parent + 1;
             f_child = g_child + h_child;
@@ -117,20 +155,12 @@ void enqueue_corner(min_heap& to_expand, const node& parent, Pyraminx& pyraminx,
             move_list child_move_list = parent.second;
             child_move_list.push_back(child_move);
             node child(f_child, child_move_list);
+            // Add to min-heap
             to_expand.push(child);
+            // Undo test to calculate cost
             pyraminx.turn_layer(ref_corner, layer, undo_dir);
         }
     }
-}
-
-bool goal_test(const node& current, Pyraminx& pyraminx){
-    // printf("Trying the following moves: ");
-    // print_move_list(current);
-    apply_moves(current, pyraminx);
-    bool solved = pyraminx.is_solved();
-    // pyraminx.print();
-    undo_moves(current, pyraminx);
-    return solved;
 }
 
 void apply_moves(const node& current, Pyraminx& pyraminx){
@@ -175,6 +205,9 @@ void print_move_list(const node& current){
         }
         if(move.dir == Direction::counterclockwise){
             dir = '`';
+        }
+        else{
+            dir = ' ';
         }
         printf("%c%i%c ", corner, layer, dir);
     }
